fd0c7a5e3f0b4769b43dea9066ff46b631cb5ef1
Alfredo Damian Palacios <adpdamy0alf@gmail.com>
2022-12-20 23:54:16 -0300

    Config pythoonAnywhere

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -115,8 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static')
 STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR),'static'),)
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
