90e3b209901688800eb9a60fef89d08607885861
Alfredo Damian Palacios <adpdamy0alf@gmail.com>
2022-12-21 00:02:20 -0300

    Prueba pythanyw

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static')
-STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR),'static'),)
+STATICFILES_DIRS = (os.path.join(os.path.dirname(BASE_DIR),'s_static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),'media')
